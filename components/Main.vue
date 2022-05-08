9683dc79df6938bf313e2c4c34bd2bda9629a9e1
99 <hahahoho5915@gmail.com>
2022-05-08 19:32:05 +0900

    기능추가: dayjs 추가

--- components/Main.vue
+++ components/Main.vue
@@ -1,7 +1,9 @@
 <template>
-  <main class="px-3">
-    <h1>Cover your page.</h1>
-    <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+  <main class="px-3 umb-content-margin umb-bg-img">
+    <h5>{{ currentDate }}</h5>
+    <h1>{{ currentTime }}</h1>
+    <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text,
+      and add your own fullscreen background photo to make it your own.</p>
     <p class="lead">
       <a href="#" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
     </p>
@@ -9,7 +11,29 @@
 </template>
 
 <script>
+import dayjs from "dayjs"
+import 'dayjs/locale/ko'
+
 export default {
   name: 'Main',
+  data() {
+    return {
+      currentDate: dayjs().format("YYYY-MM-DD (dd)"),
+      currentTime: dayjs().format("HH:mm:ss"),
+    }
+  },
+  mounted() {
+    dayjs.locale('ko')
+    let _this = this
+    this.timer = setInterval(() => {
+      _this.currentDate = dayjs().format("YYYY-MM-DD (dd)")
+      _this.currentTime = dayjs().format("HH:mm:ss")
+    }, 1000)
+  },
+  beforeDestroy() {
+    if (this.timer) {
+      clearInterval(this.timer)
+    }
+  }
 }
 </script>
--- components/Setting.vue
+++ components/Setting.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="px-3">
+  <main class="px-3 umb-content-margin">
     <h1>Setting page.</h1>
     <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
     <p class="lead">
